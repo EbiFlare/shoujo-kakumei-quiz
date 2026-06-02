c8bae996eb3517d8e24dedebd1b18a0368d0e4e4
EbiFlare <totti.likes.k66@gmail.com>
2026-06-02 20:42:49 +0900

    Update questions.js

--- js/questions.js
+++ js/questions.js
@@ -1,25 +1,463 @@
 const QUESTIONS = [
   {
-    id: "q001",
-    contributor: "テスト",
-    date: "2026-05-31",
-    question: "これはテスト問題です。正解はどれでしょう？",
-    choices: [
-      "選択肢1",
-      "選択肢2",
-      "選択肢3",
-      "選択肢4"
+    "id": "q001",
+    "contributor": "過負荷",
+    "date": "",
+    "question": "心世紀の楽曲『雑几帖』の読み方は？",
+    "choices": [
+      "ざっきちょう",
+      "ぞうきんちょう",
+      "ざつきちょう",
+      "ぞうきちょう"
     ],
-    answerIndex: 0,
-    sourceUrls: [
-      "https://www.youtube.com/"
+    "answerIndex": 0,
+    "sourceUrls": [
+      "https:
     ],
-    image: "",
-    members: [
+    "image": "",
+    "members": [
+      "心世紀"
+    ],
+    "difficulty": "easy",
+    "genre": "楽曲",
+    "explanation": "雑記帳（ざっきちょう）とは、一般には他人が読んでも構わない程度の個人の感想や意見などを記載するものである。"
+  },
+  {
+    "id": "q002",
+    "contributor": "過負荷",
+    "date": "",
+    "question": "罪十罰の楽曲『アウフヘーベン』のサビで繰り返される象徴的なフレーズは何？",
+    "choices": [
+      "find me,thank you.",
+      "find me,store.",
+      "find me,take me out.",
+      "find me,take me away."
+    ],
+    "answerIndex": 2,
+    "sourceUrls": [
+      "https://youtu.be/cZz-GuWiNnA?si=DlvRuhJuZ34dtL1U"
+    ],
+    "image": "",
+    "members": [
+      "罪十罰"
+    ],
+    "difficulty": "normal",
+    "genre": "楽曲",
+    "explanation": "和訳では「私を見つけて、連れ出して」。FINDME STOREはご存じ株式会社THINKRのECサイト。"
+  },
+  {
+    "id": "q003",
+    "contributor": "びぐ島",
+    "date": "2025-01-15",
+    "question": "これは何の絵？",
+    "choices": [
+      "悲しそうネズミ",
+      "苦しむイルカ",
+      "泣いてるシャーク",
+      "ただのバケモノ"
+    ],
+    "answerIndex": 2,
+    "sourceUrls": [
+      "https://www.youtube.com/live/p77HTJi1RPE?si=JHDjFamaaNZwobEX&t=1722"
+    ],
+    "image": "",
+    "members": [
+      "佳鏡院"
+    ],
+    "difficulty": "easy",
+    "genre": "配信",
+    "explanation": "佳鏡院は画伯です。"
+  },
+  {
+    "id": "q004",
+    "contributor": "過負荷",
+    "date": "",
+    "question": "罪十罰の楽曲『弔花』『大罪』に共通するコンポーザーの名前は？",
+    "choices": [
+      "梓川",
+      "他人事",
+      "Zexnum",
+      "たなか"
+    ],
+    "answerIndex": 1,
+    "sourceUrls": [
+      "https://youtu.be/BDZeby1Cp1c?si=Dxoonb6GfZXR7BZf",
+      "https://youtu.be/HIciS5SevlU?si=mSgQvIGvenCGjv1I"
+    ],
+    "image": "",
+    "members": [
+      "罪十罰"
+    ],
+    "difficulty": "normal",
+    "genre": "楽曲",
+    "explanation": "他に御莉姫の楽曲『Talking Doll』やV.W.Pの楽曲『真偽』なども手掛けている。"
+  },
+  {
+    "id": "q005",
+    "contributor": "過負荷",
+    "date": "",
+    "question": "少女革命計画の楽曲を多く手掛けているソングライター矢野達也が制作に関わっていない楽曲は次のうちどれ？",
+    "choices": [
+      "暴力的イグノランス",
+      "ミリオン・コンプレクシティ",
+      "クロマティック feat.ヰ世界情緒",
+      "DIGGER"
+    ],
+    "answerIndex": 3,
+    "sourceUrls": [
+      "https://youtu.be/e8cU7rcN7kk?si=tzuU11z5QFivVAz4"
+    ],
+    "image": "",
+    "members": [
       "少女革命計画"
     ],
-    difficulty: "easy",
-    genre: "テスト",
-    explanation: "これは表示確認用のテスト問題です。"
+    "difficulty": "normal",
+    "genre": "楽曲",
+    "explanation": "『DIGGER』作詞,作曲,編曲：biz・ZERA"
+  },
+  {
+    "id": "q006",
+    "contributor": "過負荷",
+    "date": "",
+    "question": "罪十罰の楽曲『Synapse』がリリースした際に使用された、曲の感想を投稿するハッシュタグは何？",
+    "choices": [
+      "#しなとぷす",
+      "#シナとプス",
+      "#Syna×pse",
+      "#シナ十プス"
+    ],
+    "answerIndex": 1,
+    "sourceUrls": [
+      "https://x.com/mikoto_gr/status/1884471551145435533?s=20"
+    ],
+    "image": "",
+    "members": [
+      "罪十罰"
+    ],
+    "difficulty": "hard",
+    "genre": "楽曲",
+    "explanation": "当時は活動初期だったこともあり楽曲リリースが一大イベントとして扱われていた。"
+  },
+  {
+    "id": "q007",
+    "contributor": "過負荷",
+    "date": "",
+    "question": "美古途ｻﾝがIBAに訪れた際に座った座席の番号は何？",
+    "choices": [
+      "9番",
+      "10番",
+      "11番",
+      "12番"
+    ],
+    "answerIndex": 0,
+    "sourceUrls": [
+      "https://x.com/mikoto_gr/status/1884471551145435533?s=20"
+    ],
+    "image": "",
+    "members": [
+      "美古途"
+    ],
+    "difficulty": "hard",
+    "genre": "IBA",
+    "explanation": "注文したメニューはヴィッテの爆釣♡ぷりん㌨！。"
+  },
+  {
+    "id": "q008",
+    "contributor": "過負荷",
+    "date": "",
+    "question": "イベント会場等で展示される少女革命計画のパネルのうち、美古途の身長はいくつ？",
+    "choices": [
+      "145cm",
+      "150cm",
+      "155cm",
+      "160cm"
+    ],
+    "answerIndex": 2,
+    "sourceUrls": [
+      "https://x.com/KAFka1192/status/1913511488586940720?s=20"
+    ],
+    "image": "",
+    "members": [
+      "美古途"
+    ],
+    "difficulty": "革命",
+    "genre": "イベント",
+    "explanation": "アホ毛単体は5cmあるため加味すると身長160cm。"
+  },
+  {
+    "id": "q009",
+    "contributor": "えびフレア",
+    "date": "",
+    "question": "夕凪機が2025年8月辺りにハマってたチーかまのメーカーは？",
+    "choices": [
+      "丸善",
+      "なとり",
+      "ニッスイ",
+      "美幸ハム"
+    ],
+    "answerIndex": 1,
+    "sourceUrls": [
+      "https://x.com/yunagi_gr/status/1957512610141859923?s=20"
+    ],
+    "image": "",
+    "members": [
+      "夕凪機"
+    ],
+    "difficulty": "革命",
+    "genre": "",
+    "explanation": "夕凪機が食べていたチーかまは35kcal、なとり35kcal、丸善53kcal、ニッスイ28kcal、美幸は存在しません。"
+  },
+  {
+    "id": "q010",
+    "contributor": "過負荷",
+    "date": "",
+    "question": "【歌ってみた】どっちにするの？ covered by 御莉姫のMVで発生している映像ミスは何？",
+    "choices": [
+      "歌詞に誤植がある",
+      "背景映像に誤植がある",
+      "クレジットに誤植がある",
+      "御莉姫の髪色が違う"
+    ],
+    "answerIndex": 1,
+    "sourceUrls": [
+      "https://youtu.be/PqqmwRR1TFM?si=azSJdieeuXxoO4xT&t=42"
+    ],
+    "image": "",
+    "members": [
+      "御莉姫"
+    ],
+    "difficulty": "革命",
+    "genre": "歌ってみた",
+    "explanation": "12種のイラスト差分に番号が振られて並んでいるシーンで11番が飛んでいる。(原曲のMVでは11番が存在する)"
+  },
+  {
+    "id": "q011",
+    "contributor": "過負荷",
+    "date": "",
+    "question": "少女革命計画のTikTok公式アカウントで最初に投稿された楽曲は何？",
+    "choices": [
+      "弔花",
+      "フェイクナイト・シンデレラ",
+      "現世回帰",
+      "well"
+    ],
+    "answerIndex": 1,
+    "sourceUrls": [
+      "https://www.tiktok.com/@girls_rev_pj?is_from_webapp=1&sender_device=pc"
+    ],
+    "image": "",
+    "members": [
+      "少女革命計画"
+    ],
+    "difficulty": "hard",
+    "genre": "SNS",
+    "explanation": "フォローといいね、よろしくお願いします。"
+  },
+  {
+    "id": "q012",
+    "contributor": "過負荷",
+    "date": "",
+    "question": "硝子宮が就寝前に投稿するポストで登場する「おやすみ」の合言葉は何？",
+    "choices": [
+      "おやちゅみや",
+      "おやすみや",
+      "ぐんないみゃ",
+      "がらすりーぴん"
+    ],
+    "answerIndex": 0,
+    "sourceUrls": [
+      "https://x.com/garasumiya_gr/status/1942225520407846974?s=20"
+    ],
+    "image": "",
+    "members": [
+      "硝子宮"
+    ],
+    "difficulty": "easy",
+    "genre": "SNS",
+    "explanation": "最初におやちゅみやがポストされたのは2025年7月7日。"
+  },
+  {
+    "id": "q013",
+    "contributor": "えびフレア",
+    "date": "",
+    "question": "weibo限定で公開されている、かきょげし鎌倉のお昼ご飯はどれ？",
+    "choices": [
+      "鎌倉野菜",
+      "カレー",
+      "海鮮",
+      "鎌倉パスタ"
+    ],
+    "answerIndex": 2,
+    "sourceUrls": [
+      "https://weibo.com/7974246617/PxpKStMrx"
+    ],
+    "image": "",
+    "members": [
+      "佳鏡院"
+    ],
+    "difficulty": "hard",
+    "genre": "SNS",
+    "explanation": "weiboだけで公開されている情報もあるとか、ないとか。"
+  },
+  {
+    "id": "q015",
+    "contributor": "えびフレア",
+    "date": "",
+    "question": "夕凪機が好きな歌、ジュウナナ！",
+    "choices": [
+      "ちゅ、多様性",
+      "私論理",
+      "アポリア",
+      "ソラニン"
+    ],
+    "answerIndex": 2,
+    "sourceUrls": [
+      "https://youtu.be/j9Sycr6o-mY?si=1SuL2f1Jvo8-Sgcu"
+    ],
+    "image": "",
+    "members": [
+      "夕凪機"
+    ],
+    "difficulty": "hard",
+    "genre": "歌ってみた",
+    "explanation": "ジュウナナ！のウナナ！の部分、カワイイですよね。"
+  },
+  {
+    "id": "q017",
+    "contributor": "過負荷",
+    "date": "",
+    "question": "少女革命計画6人のYouTubeから公開されている動画で現在最も再生されている歌は何？　(2026年5/23時点)",
+    "choices": [
+      "佳鏡院『炜WARD ROMANCE』",
+      "美古途『ピルグリム』",
+      "氷夏至『不埒な喝采』",
+      "氷夏至『でもね』"
+    ],
+    "answerIndex": 0,
+    "sourceUrls": [
+      "https://youtu.be/ZrpNjL-LDRM?si=2cgvLFujyWjhompA"
+    ],
+    "image": "",
+    "members": [
+      "少女革命計画"
+    ],
+    "difficulty": "easy",
+    "genre": "歌ってみた",
+    "explanation": "佳鏡院『炜WARD ROMANCE』159万回再生、氷夏至『不埒な喝采』60万回再生、美古途『ピルグリム』57万回再生、氷夏至『でもね』56万回再生"
+  },
+  {
+    "id": "q019",
+    "contributor": "過負荷",
+    "date": "",
+    "question": "形態、メンバー、名称など詳細な情報が明かされないまま初出演し、少女革命計画の正式なアーティストデビューを飾る舞台となったKAMITSUBAKI STUDIOの音楽イベントは何？",
+    "choices": [
+      "KAMITSUBAKI FES '23",
+      "KAMITSUBAKI FES '24",
+      "KAMITSUBAKI FES '25",
+      "KAMITSUBAKI FES '26"
+    ],
+    "answerIndex": 1,
+    "sourceUrls": [
+      "https://kamitsubaki.jp/news/2024/08/08/3712/"
+    ],
+    "image": "",
+    "members": [
+      "少女革命計画"
+    ],
+    "difficulty": "easy",
+    "genre": "ライブ",
+    "explanation": "Xtuberという形態で活動することが明かされていなかった心世紀。誰もが予想していなかったリアルの姿でのパフォーマンスは観客を大きく盛り上げた。"
+  },
+  {
+    "id": "q020",
+    "contributor": "過負荷",
+    "date": "",
+    "question": "少女革命計画メンバー6人でオリジナル楽曲をカバーしたこともある、同じく神椿に所属するアーティストは誰？",
+    "choices": [
+      "V.W.P",
+      "Albemuth",
+      "te'resa",
+      "VALIS"
+    ],
+    "answerIndex": 3,
+    "sourceUrls": [
+      "https://youtu.be/63xVryg2CUs?si=B6TK-2WEoq9vmHMV"
+    ],
+    "image": "",
+    "members": [
+      "少女革命計画"
+    ],
+    "difficulty": "easy",
+    "genre": "歌ってみた",
+    "explanation": "IBAコラボや心世紀の振り付け担当など少女革命計画とVALISは関わりが何かと多い。"
+  },
+  {
+    "id": "q021",
+    "contributor": "えびフレア",
+    "date": "",
+    "question": "佳鏡院が燃やしているのは？",
+    "choices": [
+      "松明",
+      "袖",
+      "SNS",
+      "情熱"
+    ],
+    "answerIndex": 1,
+    "sourceUrls": [
+      "https://youtu.be/JBS61ULFe48?si=l0TAYqRi3GJKwOxd"
+    ],
+    "image": "",
+    "members": [
+      "佳鏡院"
+    ],
+    "difficulty": "easy",
+    "genre": "",
+    "explanation": "きっと情熱も燃やしてくれてる。"
+  },
+  {
+    "id": "q031",
+    "contributor": "びぐ島",
+    "date": "",
+    "question": "夕凪機のあだ名「うなぎ」を命名した人は誰？",
+    "choices": [
+      "氷夏至",
+      "美古途",
+      "マネージャー",
+      "PIEDPIPER"
+    ],
+    "answerIndex": 1,
+    "sourceUrls": [
+      "https://www.youtube.com/live/4Mry1kf2lR4?si=c3Mp29eA2suLm9HW&t=766"
+    ],
+    "image": "",
+    "members": [
+      "夕凪機"
+    ],
+    "difficulty": "normal",
+    "genre": "配信",
+    "explanation": "フェスにて、美古途が夕凪機に対して「『ゆうなぎ』って『うなぎ』入ってるよね」と言い、うなぎ呼びが始まった。"
+  },
+  {
+    "id": "q032",
+    "contributor": "びぐ島",
+    "date": "",
+    "question": "氷夏至が惹かれている体の特徴は？",
+    "choices": [
+      "大きい目",
+      "長い脚",
+      "細い指",
+      "尖った顎"
+    ],
+    "answerIndex": 3,
+    "sourceUrls": [
+      "https://www.youtube.com/live/Dt5Y1Nj9Npk?si=PXSPLx3-3fFXjYdy&t=10781"
+    ],
+    "image": "",
+    "members": [
+      "氷夏至"
+    ],
+    "difficulty": "easy",
+    "genre": "配信",
+    "explanation": "氷夏至は学園ハンサムやパーフェクト城之内のような尖った顎が大好き。"
   }
 ];
